6a3c518c44571a747e4a97e697f6588aa54fde2b
PrajaktaKeer <prajaktakeer01@gmail.com>
2020-12-18 19:54:53 +0530

    scrapped all cards

--- mitre/mitre/spiders/metrics_spider.py
+++ mitre/mitre/spiders/metrics_spider.py
@@ -1,17 +1,57 @@
 import scrapy
+import re
+import string 
 from ..items import MitreItem
 
 class MetricsSpider(scrapy.Spider):
-	name = 'metrics'
+	name = 'metrics'  #name of the spider
 	start_urls = [
-		'https://attack.mitre.org/'
+		'https://attack.mitre.org/'  #url which we want to scrap
 	]
 
 	def parse(self, response):
-		# title = response.css('title::text').extract()
-		# yield {'titletext' : title}
-		items = MitreItem()
-		tactics = response.xpath("//td[@class = 'tactic name']").css('a::text').extract()
-		#yield {'Tactics' : tactics}
-		items['tactic_name'] = tactics
-		yield items
+		#title = response.css('title::text').extract()
+		#yield {'titletext' : title}
+		# items = MitreItem() #creating object of MitreItem class
+		# tactics = response.xpath("//td[@class = 'tactic name']").css('a::text').extract()
+		# #yield {'Tactics' : tactics}
+		# items['tactic_name'] = tactics
+		# yield items
+
+		url = response.css('div.technique-cell a::attr(href)').extract()
+		for item in url:
+			yield response.follow(item, callback = self.parse_dir_contents)
+
+	def parse_dir_contents(self, response):
+	    other = response.xpath("//div[@class = 'card-data']/text()").extract()
+	    id = response.css('[id="card-id"]::text').extract()
+	    sub_technique = response.xpath("//div[@class = 'card-data']//a/text()").extract()
+	    tactic = response.css('[id="card-tactics"]::text').extract()
+	    platform = response.css('[id="card-platforms"]::text').extract()
+	    technique_name = response.css('h1::text').extract()
+
+	    tactic = "".join(tactic)
+	    tactic = tactic.replace(" ", "")
+	    tactic = tactic.replace("\n", "")
+	    technique_name = "".join(technique_name)
+	    technique_name = technique_name.replace(" ", "")
+	    technique_name = technique_name.replace("\n", "")
+
+	    items = MitreItem() #creating object of MitreItem class
+	    items['id'] = "".join(id[0])
+	    items['sub_technique'] = sub_technique
+	    items['tactic'] = tactic
+	    items['platform'] = "".join(platform[0])
+	    items['technique_name'] = technique_name
+	    items['data_sources'] = other[-4]
+	    items['version'] = other[-3]
+	    items['created'] = other[-2]
+	    items['last_modified'] = other[-1]
+	    yield items
+
+	    #yield {'Technique' : technique_name, 'Id' : id, 'Sub Techniques' : sub_technique, 'Tactic' : tactic, 'Platform' : platform, 'Data Sources' : other[-4], 'Version' : other[-3], 'Created' : other[-2], 'Last Modified' : other[-1]}
+
+
+
+
+		
