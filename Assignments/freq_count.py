935c4e3ccd8d6252c40ecbfebc20dd29fc3eb52d
PrajaktaKeer <prajaktakeer01@gmail.com>
2020-12-27 17:23:03 +0530

    solved assignment questions

--- Assignments/freq_count.py
+++ Assignments/freq_count.py
@@ -9,20 +9,25 @@
 
 import collections
 
-words = ["hello", "hi", "hello", "good", "bad", "good"]
+words = []
+#accepting user input
+size = int(input("How many elements do you want to enter? "))
+for i in range(1, size+1):
+	words.append(str(input("Enter element " + str(i) + " ")))
 unique = []
 
 #using built in library
 counter = collections.Counter(words)
+print("---------------------------------------")
 print("Method 1")
 print(counter)
-print()
+print("---------------------------------------")
 print("Method 2")
 
 #manual method
 for word in words:
 	if word not in unique:
-		unique.append(word)
+		unique.append(word) #finding all the unique words
 
 for word in unique:
 	print(word, " : ", words.count(word))
--- odd_even.py
+++
@@ -1,6 +0,0 @@
-# Arrange the odd and even separately Even one’s first,odd ones later
-# 1,3,2,5,6,4,7,8,9
-# Output will be odd 1st and then even.
-
-a = [1, 3, 2, 5, 6, 4, 7, 8]
-
