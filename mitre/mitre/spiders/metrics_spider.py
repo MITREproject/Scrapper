4b9031f0e7959b83f2a01feb69b0b0fd629b8eeb
PrajaktaKeer <prajaktakeer01@gmail.com>
2021-05-12 12:16:00 +0530

    updated spider count 552

--- mitre/mitre/spiders/metrics_spider.py
+++ mitre/mitre/spiders/metrics_spider.py
@@ -4,100 +4,66 @@ import string
 from ..items import MitreItem
 
 class MetricsSpider(scrapy.Spider):
-	name = 'metrics2'  #name of the spider
+	name = 'metrics'  #name of the spider
 	start_urls = [
-		'https://attack.mitre.org/techniques/T1543/'  #url which we want to scrap
-		#'https://attack.mitre.org'
+		#'https://attack.mitre.org/techniques/T1529/'  #url which we want to scrap
+		'https://attack.mitre.org'
 	]
 
 	def parse(self, response):
-	# 	url = response.css('table.techniques-table a::attr(href)').extract()
-	# 	for item in url:
-	# 		yield response.follow(item, callback = self.parse_dir_contents)
+		url = response.css('table.techniques-table a::attr(href)').extract()
+		for item in url:
+			yield response.follow(item, callback = self.parse_dir_contents)
 
-	# def parse_dir_contents(self, response):
+	def parse_dir_contents(self, response):
 
-		cardValues = response.xpath("//div[@class = 'col-md-11 pl-0']/text()").extract() #holds values of card details
-		cardKeys = response.xpath("//span[@class = 'h5 card-title']/text()").extract() #holds keys of card details
-		subTechnique = response.xpath("//div[@class = 'card-data']//a/text()").extract() #holds sub-technique id's and CAPEC id's
-		techniqueName = response.css('h1::text').extract() #holds technique names
-		capec = [] #seperate CAPEC id's from sub_technique
 		TechniqueData = {} #to be used as dynamic item
-		idx = 0 #position of tag 'CAPEC ID' in key
 
-		detection = response.xpath("//div[@class = 'container-fluid']/div/p/text()").extract() 
-		description = response.xpath("//div[@class = 'description-body']/p/a/text()" + "|" 
-			+ "//div[@class = 'description-body']/p/text()").extract()	
-		description = "".join(description)
-		detection = "".join(detection)
+		keys = response.xpath("//span[@class = 'h5 card-title']")
+		values = response.xpath("//div[@class = 'col-md-11 pl-0']")
+		
+		print(len(keys), len(values))
+
+		for i in range(len(keys)):
+			key = keys[i].xpath('text()').extract()
+			key[0] = key[0].replace("\xa0", "")
+			value = values[i].xpath('text()' + "|" + "a/text()").extract()
+			temp_val = []
+			for j in range(len(value)):
+				value[j] = value[j].strip()
+				if not "\n" in value[j] and len(value[j]) > 1:
+					temp_val.append(value[j])
+			TechniqueData[key[0]] = temp_val
+
+		TechniqueData['Detection'] = "".join(response.xpath("//div[@class = 'container-fluid']/div/p/text()").extract())
+		TechniqueData['Description'] = "".join(response.xpath("//div[@class = 'description-body']/p/a/text()" + "|" 
+			+ "//div[@class = 'description-body']/p/text()").extract())	
+
 		Mitigations = {}
 		ProcedureExamples = {}		    
 
 		tables = response.xpath("//table[@class = 'table table-bordered table-alternate mt-2']")
 
 		for i in range(len(tables)):
-			type = tables[i].xpath('thead/tr/th/text()').extract_first()
+			type = tables[i].xpath('thead/tr/th/text()').extract()[1]
 			for row in tables[i].xpath('tbody/tr'):
-				keys = row.xpath('td/a/text()')[0].extract()
+				keys = row.xpath('td/a/text()')[0].extract() 
+				mid_values = row.xpath('td/a/text()')[1].extract()
 				values = row.xpath("td/p/text()" + "|" + "td/p/a/text()").extract()
 				values = "".join(values)
+				keys=''.join(e for e in keys if e.isalnum())
 				if type == "Name":
-					ProcedureExamples[keys.strip()] = values
+					ProcedureExamples[keys.strip().lower()] = mid_values + " : " + values
 				elif type == "Mitigation":
-					Mitigations[keys.strip()] = values
+					Mitigations[keys.strip().lower()] = mid_values + " : " + values
 
-		TechniqueData['Detection'] = detection
-		TechniqueData['Description'] = description
 		if Mitigations:
-			TechniqueData['Mitigations'] = Mitigations
+			TechniqueData['mitigations'] = Mitigations
 		if ProcedureExamples:
-			TechniqueData['Procedure Examples'] = ProcedureExamples
-
-		#remove leading and trailing spaces from technique name and remove unwanted symbols
-		techniqueName = "".join(techniqueName).strip().replace("\n", "")
-
-		#initializing technique name
-		TechniqueData['Technique Name'] = techniqueName
-
-		#formatting the keys. Removing trailing spaces and colon
-		for i in range(len(cardKeys)):
-			cardKeys[i] = cardKeys[i].strip()
-			cardKeys[i] = cardKeys[i][:-1]
-			if cardKeys[i] == 'CAPEC ID':
-				idx = i
-
-		#formatting the values.Removing unwanted elements
-		for i in range(len(cardValues)):
-			cardValues[i] = cardValues[i].strip()
-			if "\n" in cardValues[i]:
-				cardValues.remove(cardValues[i])
-
-		#eliminating blank elements in values
-		for elem in cardValues[:]:
-			if len(elem) <= 1:
-				cardValues.remove(elem)
-
-		#seperating CAPEC id's from sub-technique id's
-		for elem in subTechnique[:]:
-			if "CAPEC" in elem:
-				capec.append(elem)
-				subTechnique.remove(elem)
-
-		#initializing sub-technique id's
-		if len(subTechnique) == 1:
-			cardValues.insert(1, subTechnique[0])
-		if len(subTechnique) > 1:
-			cardValues.insert(1, subTechnique)
-
-		#initializing capec id's
-		if len(capec) == 1:
-			cardValues.insert(idx, capec[0])
-		if idx != 0 and len(capec) > 1: 
-			cardValues.insert(idx, capec) 
-
-		#initializing other attributes
-		for i in range(len(cardKeys)):
-			TechniqueData[cardKeys[i]] = cardValues[i] 
+			TechniqueData['procedureexamples'] = ProcedureExamples
+			if not Mitigations:
+				Mitigations['mitigations'] = response.xpath("//div[@class = 'container-fluid']/p[not(@scite-citeref-number)]/text()").extract()[0].strip() 
+				TechniqueData['mitigations']= Mitigations
 
 		#returning the created dictionary
 		yield MitreItem( **TechniqueData )
